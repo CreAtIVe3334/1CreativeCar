2aa2dce84a5c1ddec4b68083f888fdd592d02b9e
Huseyn Memmedov <106882652+CreAtIVe3334@users.noreply.github.com>
2024-08-09 02:28:40 -0700

    “v1 updated”

--- src/main/java/com/example/CreativeCar/entity/Order.java
+++ src/main/java/com/example/CreativeCar/entity/Order.java
@@ -1,23 +1,21 @@
 package com.example.CreativeCar.entity;
 
+import com.example.CreativeCar.utility.core_entity.CoreEntity;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 
 @Entity
+@SuperBuilder
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-@Builder
-@ToString
+@NoArgsConstructor
 @Table(name = "rental_order")
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class Order {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    Long id;
+public class Order extends CoreEntity {
 
     LocalDateTime startTime;
 
